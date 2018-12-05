8cf5910e25972c88830c6a17a6baa16f511f02c6
Matthew Wynn <matthew@matthewwynn.com>
2018-12-04 20:46:38 -0600

    Day 1

---
+++ day1/src/main.rs
@@ -0,0 +1,36 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::collections::BTreeSet;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    assert!(args.len() == 2);
+    let mut f = File::open(&args[1]).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    println!("Frequency: {}", part_1(&contents));
+    println!("First reached twice: {}", part_2(&contents).unwrap());
+
+}
+
+fn part_1(input: &str) -> i32 {
+    input.lines().map(|s| s.parse::<i32>().unwrap()).sum()
+}
+
+
+fn part_2(input: &str) -> Option<i32> {
+    let num_iterator = input.lines().map(|s| s.parse::<i32>().unwrap());
+    let mut seen = BTreeSet::new();
+    let mut acc = 0;
+    for i in num_iterator.cycle() {
+        acc += i;
+        if !seen.insert(acc) {
+            return Some(acc)
+        }
+    }
+    None
+}
